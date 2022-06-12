16bece1a4d86de0d38b2d0c67f5e5349da379ca6
0kina <itokon20@gmail.com>
2022-06-12 22:21:25 +0900

    EvalML1を更新

--- EvalML1/main.py
+++ EvalML1/main.py
@@ -3,7 +3,7 @@ import sys
 from lark import Lark, Tree, Token
 
 from helper import pretty
-from eval_ml1 import EvalEvalML1, DerivateEvalNatExp
+from eval_ml1 import DerivateEvalNatExp
 
 if __name__ == '__main__':
     parser = Lark.open("eval_ml1.lark", rel_to=__file__, lexer='basic')
